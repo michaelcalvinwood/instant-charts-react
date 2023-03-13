23b993db8dac7f97890aecbb5974ed28c6dbcd40
Michael Wood <michaelwood33311@icloud.com>
2023-03-13 11:35:25 -0400

    added dropzone for uploading files

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,11 +1,32 @@
 import './FileUpload.scss';
 import React from 'react';
+import Dropzone from 'react-dropzone';
+
 
 const FileUpload = () => {
+    const uploadFiles = files => {
+        console.log('the files', files);
+    }
 
     return (
         <div className="file-upload">
-            File Upload
+           <div 
+                className="dropzone-container"
+                style={{cursor: 'pointer', lineHeight: '1.3', padding: ".25rem .5rem", margin: '1rem auto 0 auto', width: '80%', display: 'block', height: '4rem', border: '2px solid blue', borderRadius: '4px'}}
+                
+            >
+            <Dropzone 
+                onDrop={acceptedFiles => uploadFiles(acceptedFiles)}>
+                    {({getRootProps, getInputProps}) => (
+                        <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag 'n' drop some files here.<br /> Or click to select files</p>
+                        </div>
+                        </section>
+                    )}
+                </Dropzone>
+           </div>
         </div>
         
     )

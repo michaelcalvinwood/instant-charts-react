49003e67f93b0e44a4c290697f998047f3b81cec
Michael Wood <michaelwood33311@icloud.com>
2023-03-13 08:53:07 -0400

    added empty FileUpload component

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,13 @@
+import { upload } from "@testing-library/user-event/dist/upload"
+
+const FileUpload = () => {
+
+    return (
+        <div className="file-upload">
+            Hello File Upload
+        </div>
+        
+    )
+}
+
+export default FileUpload;

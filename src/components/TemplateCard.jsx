fb7adb3766a82bb77cb745453a961ddff34b9100
Michael Wood <michaelwood33311@icloud.com>
2023-03-14 09:01:34 -0400

    added TemplateCard component placeholder

---
+++ src/components/TemplateCard.jsx
@@ -0,0 +1,12 @@
+import './TemplateCard.scss';
+import React from 'react';
+
+function TemplateCard({selection}) {
+  return (
+    <div>
+        {selection}
+    </div>
+  )
+}
+
+export default TemplateCard
--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import template from '../templates/template';
 
 const Templates = ({templates, setTemplates, chart}) => {
-  console.log('template', template);
+  console.log('template', template, Object.keys(template));
+
+
   return (
     <div className='templates'>
       <h2 className='templates--heading'>
           Templates for {chart.charAt(0).toUpperCase() + chart.slice(1)} Charts
       </h2>
+      <div className="templates--selections">
+        {!templates[chart] && <p>No Templates Found</p>} 
+      </div>
     </div>
   )
 }

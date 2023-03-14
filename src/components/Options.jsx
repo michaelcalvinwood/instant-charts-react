2eb43d253ccd6b72580c47330ce3e23b90917061
Michael Wood <michaelwood33311@icloud.com>
2023-03-14 11:58:27 -0400

    styled Options component

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -18,17 +18,21 @@ function Options() {
   return (
     <div className='options'>
       <h2 className="options--heading">Options</h2>
-      <label htmlFor="chartTitle" className="options--chart-title-label">
-          Title:
-          <input type="text" name="chartTitle" id="chartTitle" className="options--chart-title" value='' onChange={handleTitle}/>
-      </label>
-      <label htmlFor="chartSubtitle" className="options--chart-subtitle-label">
-        Subtitle:
-        <input type="text" name="chartSubtitle" id="chartSubtitle" className="options--chart-subtitle" value='' onChange={handleSubtitle}/>
-      </label>
-      <div>Legend: <input type="checkbox" name="chartLegend" id="chartLegend" checked onChange={handleLegend}/></div>
-      <div>Color:</div>
-      <div>Color list goes here</div>
+      
+      <div className='options--chart-label'>Title:</div>
+      <input type="text" name="chartTitle" id="chartTitle" className="options--chart-title" value='' onChange={handleTitle}/>
+      <br />
+    
+      <div className='options--chart-label'>Subtitle:</div>
+      <input type="text" name="chartSubtitle" id="chartSubtitle" className="options--chart-subtitle" value='' onChange={handleSubtitle}/>
+      <br />
+      
+      <div className='options--chart-label'>Legend:</div> 
+      <input type="checkbox" name="chartLegend" id="chartLegend" checked onChange={handleLegend}/>
+      <br />
+
+      <div className='options--chart-label'>Color:</div>
+      <div className='options--color-list'>Color list goes here</div>
     </div>
   )
 }
